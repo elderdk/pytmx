565102a0910214c5a1116d360b379c0ea3f5b55a
Daniel K Elder <elder.dk@gmail.com>
2021-12-29 20:43:25 +0900

    add class and write readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pytmx
 Python-written tool for handling tmx files
+
+
+Example:
+
+In pytmx.py, fill in the variables with information accurate to your use case and run PYTMX.excel_to_tmx()
+
+    filepath = r""
+    srclang = "en-us",
+    tarlang = "ko-kr"
+
+    pytmx = PYTMX(filepath, srclang, tarlang)
+
+    pytmx.excel_to_tmx()
--- pytmx.py
+++ pytmx.py
@@ -2,13 +2,29 @@ from xls_to_tmx.xls_to_tmx import convert
 from pathlib import Path
 
 
+class PYTMX:
+
+    def __init__(self,filepath, srclang, tarlang):
+        self.filepath = filepath
+        self.srclang = srclang
+        self.tarlang = tarlang
+
+    def excel_to_tmx(self):
+
+        dict = {
+        'tmname': Path(self.filepath).stem + ' TM',
+        'srclang': self.srclang,
+        'tarlang': self.tarlang,
+        }
+
+        convert(self.filepath, **dict)
+
 if __name__=='__main__':
-    excel_file = r""
 
-    dict = {
-    'tmname': Path(excel_file).stem + ' TM',
-    'srclang': 'ko-kr',
-    'tarlang': 'en-us',
-    }
+    filepath = r""
+    srclang = "en-us",
+    tarlang = "ko-kr"
 
-    convert(excel_file, **dict)
+    pytmx = PYTMX(filepath, srclang, tarlang)
+
+    pytmx.excel_to_tmx()
